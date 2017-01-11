800cd0aaec4ed1d2a992dbfd656fb29b39627dea
antoinevastel <antoine.vastel@gmail.com>
2017-01-11 16:53:32 +0100

    adds tests

---
+++ tests/testTask.py
@@ -0,0 +1,12 @@
+import json
+
+from scheduler.task import Task
+
+
+class TestTask(Task):
+
+    def __init__(self, json_definition):
+        super(TestTask, self).__init__(json_definition)
+
+    def run(self):
+        return ""
